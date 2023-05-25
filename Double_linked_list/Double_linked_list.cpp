57c6648b4946169269a5a0a8dc3b54e3e9badfc7
Amandaptr44 <amandaptr86@gmail.com>
2023-05-25 11:27:47 +0700

    menyelesaikan fungsi search di class DoubleLinkedList

--- Double_linked_list/Double_linked_list.cpp
+++ Double_linked_list/Double_linked_list.cpp
@@ -69,10 +69,16 @@ void DoubleLinkedList::addNode() {
 	if (current->next != NULL)
 		current->next->prev = newNode;
 	current->next = newNode;
-
-
-	bool DoubleLinkedList::search(int rollNo, Node * *previous, Node * *current) {
-
-	}
 }
+
+	bool DoubleLinkedList::search(int rollNo, Node **previous, Node **current) {
+		*previous = *current = START;
+		while (*current != NULL && rollNo != (*current)->noMhs) {
+			*previous = *current;
+			*current = (*current)->next;
+		}
+		return (*current != NULL);
+	}
+
+
 
